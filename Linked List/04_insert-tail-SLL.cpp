7c6f2ba2af7ef4545cffe38cd21df9fe5411bac4
aniketkumargiri <aniket.kumar.giri2707@gmail.com>
2022-07-09 09:25:33 +0530

    dsa 01

---
+++ Linked List/04_insert-tail-SLL.cpp
@@ -0,0 +1,72 @@
+// Singly Linked List: insert an element at the tail position
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next; // address to next node
+
+    // constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAtHead(Node *&head, int d)
+{
+    // new node create
+    Node *temp = new Node(d);
+    temp->next = head;
+    head = temp;
+}
+
+void insertAtTail(Node *&tail, int d)
+{
+    // new node create
+    Node *temp = new Node(d);
+    tail->next = temp;
+    tail = temp;
+}
+
+void print(Node *&head)
+{
+
+    if (head == NULL)
+    {
+        cout << "List is empty " << endl;
+        return;
+    }
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // created a new node
+    Node *node1 = new Node(10);
+    // cout << node1 -> data << endl;
+    // cout << node1 -> next << endl;
+
+    // head pointed to node1
+    Node *head = node1;
+    Node *tail = node1;
+    print(head);
+
+    insertAtTail(tail, 12);
+    print(head);
+
+    insertAtTail(tail, 15);
+    print(head);
+
+    return 0;
+}
